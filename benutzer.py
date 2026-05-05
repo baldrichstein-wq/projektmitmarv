8d75b805dcb7082452ea18bed2d73b81dbb5a547
baldrichstein-wq <baldrichstein@googlemail.com>
2026-05-05 09:23:24 +0200

    neu

--- benutzer.py
+++ benutzer.py
@@ -1,22 +1,15 @@
 import sqlite3
 #1. Datenbank Verbindung herstellen und erstellen falls nicht vorhanden
-conn = sqlite3.connect('rezept_datenbank.db')
+conn = sqlite3.connect('benutzer.db')
 cursor = conn.cursor()
 #2. Tabellen erstellen
 cursor.execute('''
-               CREATE TABLE IF NOT EXISTS rezepte(
-                   id INTEGER PRIMARY KEY AUTOINCREMENT,
-                   name TEXT NOT NULL UNIQUE
-               )
-               ''')
-
-cursor.execute('''
-               CREATE TABLE IF NOT EXISTS zutaten (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    rezept_id INTEGER,
-                    zutaten_name TEXT NOT NULL,
-                    menge REAL NOT NULL,
-                    FOREIGEN KEY (rezept_id) REFERENCES rezepte(id)
-                    )
-               ''')
+    CREATE TABLE IF NOT EXISTS benutzer (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL
+    )
+''')
 conn.commit()
+conn.close()
