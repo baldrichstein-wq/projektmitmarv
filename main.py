2242cdd3d36feede636d85db1e6ea50fd2905120
baldrichstein-wq <baldrichstein@googlemail.com>
2026-05-05 09:22:10 +0200

    neu

---
+++ main.py
@@ -0,0 +1,8 @@
+import fastapi
+import uvicorn
+from benutzer import app
+from wines.py import app
+from essen.py import app
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
